d65dcd2d5a671478b1e5e606111c8d98418062ba
zeonal01 <zeon.al951@gmail.com>
2024-11-12 13:12:10 +0100

    Add files via upload

---
+++ public/build/app.652303d0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[524],{28:()=>{console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉")}},s=>{var e;e=28,s(s.s=e)}]);
